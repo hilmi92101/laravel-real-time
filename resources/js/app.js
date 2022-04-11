766a3a1f245da7289f34b4a7a8a4ab675adb4d6e
hilmi <muhammadhilmi92101@gmail.com>
2022-04-12 00:07:34 +0800

    setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp(ExampleComponent); 
+app.mount('#app');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+require("laravel-mix-vue3");
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
